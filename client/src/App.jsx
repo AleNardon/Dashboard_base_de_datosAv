03ff3e555825204135fe4ddeb0ad5de6770c6cdc
AleNardon <alejonardon2002@gmail.com>
2023-10-21 20:08:28 -0300

    configuracion del cliente

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
 import {BrowserRouter , Routes, Route} from 'react-router-dom';
+
+import LoginPage  from "./pages/Login";
+
 function App() {
   return (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<h1>Login</h1> }/>
       <Route path="/dashboard" element={<h1>Dashboard</h1> }/>
+      <Route path="/login" element={<LoginPage/> }/>
     </Routes>
   </BrowserRouter>
     );
