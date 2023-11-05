fbb60032093c436f0550af71cca1a15601d433ab
AleNardon <alejonardon2002@gmail.com>
2023-11-05 13:15:49 -0300

    modificacion en la consulta de usuarios

--- src/controllers/alumnos.controller.js
+++ src/controllers/alumnos.controller.js
@@ -4,8 +4,13 @@ import { pool } from "../db.js";
 export const getAlumnos = async (req, res) => {
 
     try {
-        const sqlquery = `SELECT a.alumno_id, a.nombre_apellido, a.telefono, a.activo, a.fecha_creacion, ap.primer_clase, ap.ultima_clase,(SELECT SUM(ap.monto_plan) - SUM(ap.pago_acumulado) from alumnos_planes as ap WHERE ap.alumno_id=a.alumno_id) as deuda
-                            FROM alumnos AS a inner join alumnos_planes as ap on ap.alumno_id = a.alumno_id`
+        const sqlquery = `SELECT a.alumno_id as id , a.nombre_apellido, a.telefono, a.activo, a.fecha_creacion,ap.primer_clase,ap.ultima_clase,
+        (SELECT SUM(ap.monto_plan) - SUM(ap.pago_acumulado) from alumnos_planes as ap WHERE ap.alumno_id=a.alumno_id) as deuda
+                                    FROM alumnos AS a 
+                                    LEFT JOIN (
+            SELECT ap.alumno_id,MAX(ap.primer_clase) AS primer_clase,MAX(ap.ultima_clase) AS ultima_clase
+            FROM alumnos_planes ap GROUP BY ap.alumno_id
+        ) ap ON a.alumno_id = ap.alumno_id  `
         const rows = await pool.query(sqlquery);
         res.json(rows);
     } catch (error) {
